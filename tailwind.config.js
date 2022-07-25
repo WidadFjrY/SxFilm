4022a2340325ec75b000faa9848731a59bc8e440
Widad Al Fajri <widadzdadz@gmail.com>
2022-07-25 21:27:38 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        NSW: ["Edu NSW ACT Foundation"],
+        Fira: ["Fira Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
